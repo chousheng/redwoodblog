49c951ec4b797fef2279f5a6731d8f37b44109b9
Sheng Chou <chousheng@gmail.com>
2023-05-15 10:34:06 -0700

    Pass onBlur mode to Form directly using formMethods
    The config prop is ignored if we ar eusing formMethods prop. See FormInner() in redwood/packages/forms/src/index.tsx

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -20,7 +20,7 @@ const CREATE_CONTACT = gql`
 `
 
 const ContactPage = () => {
-  const formMethods = useForm()
+  const formMethods = useForm({ mode: 'onBlur' })
 
   const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
     onCompleted: () => {
@@ -39,12 +39,7 @@ const ContactPage = () => {
 
       <Toaster />
 
-      <Form
-        onSubmit={onSubmit}
-        config={{ mode: 'onBlur' }}
-        error={error}
-        formMethods={formMethods}
-      >
+      <Form onSubmit={onSubmit} error={error} formMethods={formMethods}>
         <FormError error={error} wrapperClassName="form-error" />
         <Label name="name" errorClassName="error">
           Name
