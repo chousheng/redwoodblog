a0cf84f8bdf71b4b766ea84baff0e4d412c574cc
Sheng Chou <chousheng@gmail.com>
2023-05-22 10:48:16 -0700

    Add summary prop to Article and modify the stories

--- web/src/components/Article/Article.js
+++ web/src/components/Article/Article.js
@@ -1,6 +1,10 @@
 import { Link, routes } from '@redwoodjs/router'
 
-const Article = ({ article }) => {
+const truncate = (text, length) => {
+  return text.substring(0, length) + '...'
+}
+
+const Article = ({ article, summary = false }) => {
   return (
     <article>
       <header>
@@ -8,7 +12,10 @@ const Article = ({ article }) => {
           <Link to={routes.article({ id: article.id })}>{article.title}</Link>
         </h2>
       </header>
-      <div className="mt-2 font-light text-gray-900">{article.body}</div>
+      <div className="mt-2 font-light text-gray-900">
+        {article.body}
+        {summary ? truncate(article.body, 100) : article.body}
+      </div>
       {/* <div>Posted at: {article.createdAt}</div> */}
     </article>
   )
--- web/src/components/ArticlesCell/ArticlesCell.js
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -23,7 +23,7 @@ export const Success = ({ articles }) => {
   return (
     <div className="space-y-10">
       {articles.map((article) => (
-        <Article key={article.id} article={article} />
+        <Article key={article.id} article={article} summary={true} />
       ))}
     </div>
   )
