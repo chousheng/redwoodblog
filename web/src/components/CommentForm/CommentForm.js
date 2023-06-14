f6a5a4b7ba802da7c15354aa63c04bb3406cf979
Sheng Chou <chousheng@gmail.com>
2023-06-14 13:18:57 -0700

    Hide CommentForm after adding comment and show a Toast

--- web/src/components/CommentForm/CommentForm.js
+++ web/src/components/CommentForm/CommentForm.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import {
   Form,
   FormError,
@@ -7,6 +9,7 @@ import {
   Submit,
 } from '@redwoodjs/forms'
 import { useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/toast'
 
 import { QUERY as CommentsQuery } from 'src/components/CommentsCell'
 
@@ -22,7 +25,13 @@ const CREATE = gql`
 `
 
 const CommentForm = ({ postId }) => {
+  const [hasPosted, setHasPosted] = useState(false)
+
   const [createComment, { loading, error }] = useMutation(CREATE, {
+    onCompleted: () => {
+      setHasPosted(true)
+      toast.success('Thank you for your comment!')
+    },
     refetchQueries: [{ query: CommentsQuery }],
   })
 
@@ -31,7 +40,7 @@ const CommentForm = ({ postId }) => {
   }
 
   return (
-    <div>
+    <div className={hasPosted ? 'hidden' : ''}>
       <h3 className="text-lg font-light text-gray-600">Leave a Comment</h3>
       <Form className="mt-4 w-full" onSubmit={onSubmit}>
         <FormError
