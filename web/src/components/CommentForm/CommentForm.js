b41763b302795d5e8b97b4a2e8ea281e79a4c6ea
Sheng Chou <chousheng@gmail.com>
2023-06-13 23:07:34 -0700

    Create CommentForm

--- web/src/components/CommentForm/CommentForm.js
+++ web/src/components/CommentForm/CommentForm.js
@@ -1,8 +1,35 @@
+import { Form, Label, TextField, TextAreaField, Submit } from '@redwoodjs/forms'
+
 const CommentForm = () => {
   return (
     <div>
-      <h2>{'CommentForm'}</h2>
-      <p>{'Find me in ./web/src/components/CommentForm/CommentForm.js'}</p>
+      <h3 className="text-lg font-light text-gray-600">Leave a Comment</h3>
+      <Form className="mt-4 w-full">
+        <Label name="name" className="block text-sm uppercase text-gray-600">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          className="block w-full rounded border p-1 text-xs "
+          validation={{ required: true }}
+        />
+
+        <Label
+          name="body"
+          className="mt-4 block text-sm uppercase text-gray-600"
+        >
+          Comment
+        </Label>
+        <TextAreaField
+          name="body"
+          className="block h-24 w-full rounded border p-1 text-xs"
+          validation={{ required: true }}
+        />
+
+        <Submit className="mt-4 block rounded bg-blue-500 px-3 py-2 text-xs font-semibold uppercase tracking-wide text-white disabled:opacity-50">
+          Submit
+        </Submit>
+      </Form>
     </div>
   )
 }
