050e9ee5b71a764c89a86cd11bafca6f7adb3eb4
Sheng Chou <chousheng@gmail.com>
2023-06-15 13:09:01 -0700

    Generate sdl and services for User model
    - yarn rw g sdl User --no-crud
    - Comment out unsued fields

---
+++ api/src/services/users/users.scenarios.js
@@ -0,0 +1,14 @@
+export const standard = defineScenario({
+  user: {
+    one: {
+      data: { email: 'String974871', hashedPassword: 'String', salt: 'String' },
+    },
+    two: {
+      data: {
+        email: 'String4448204',
+        hashedPassword: 'String',
+        salt: 'String',
+      },
+    },
+  },
+})
