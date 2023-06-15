3c724ccaf6443ccad7ef8373b9d6fa23b16485a4
Sheng Chou <chousheng@gmail.com>
2023-06-14 21:49:50 -0700

    Show the delete button for the moderator role

--- web/src/components/Comment/Comment.js
+++ web/src/components/Comment/Comment.js
@@ -1,3 +1,5 @@
+import { useAuth } from 'src/auth'
+
 const formattedDate = (datetime) => {
   const parsedDate = new Date(datetime)
   const month = parsedDate.toLocaleString('default', { month: 'long' })
@@ -5,8 +7,15 @@ const formattedDate = (datetime) => {
 }
 
 const Comment = ({ comment }) => {
+  const { hasRole } = useAuth()
+  const moderate = () => {
+    if (confirm('Are you sure?')) {
+      // TODO: delete comment
+    }
+  }
+
   return (
-    <div className="rounded-lg bg-gray-200 p-8">
+    <div className="relative rounded-lg bg-gray-200 p-8">
       <header className="flex justify-between">
         <h2 className="font-semibold text-gray-700">{comment.name}</h2>
         <time className="text-xs text-gray-500" dateTime={comment.createdAt}>
@@ -14,6 +23,15 @@ const Comment = ({ comment }) => {
         </time>
       </header>
       <p className="mt-2 text-sm">{comment.body}</p>
+      {hasRole('moderator') && (
+        <button
+          type="button"
+          onClick={moderate}
+          className="absolute bottom-2 right-2 rounded bg-red-500 px-2 py-1 text-xs text-white"
+        >
+          Delete
+        </button>
+      )}
     </div>
   )
 }
